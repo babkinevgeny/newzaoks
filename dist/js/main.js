837afb2544b44d3e27fe712e55cd8ec1254ad606
Евгений Бабкин <evgenybabkin@MacBook-Pro-Evgenij.local>
2018-10-08 19:26:45 +0300

    Added jquery and fotorama through bower. Wrote tasks for ftp unploading, building and watching the project in gulpfile.js, but it was hidden from Git.

---
+++ dist/js/main.js
@@ -0,0 +1,54 @@
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+}
+
+$(document).ready(function(){
+	/*Показ меню при клике на гамбургер*/
+	$('.hamburger').click(function(){
+		if($('.main-header .navigation').is(':visible')){
+			$('.main-header .navigation').fadeOut();
+			enableScroll();
+		} else {
+			$('.main-header .navigation').fadeIn().css('display', 'flex');
+			disableScroll();
+		}
+
+	});
+	/*Закрытие меню при клике на крестик*/
+	$('.navigation__btn').click(function(){
+	  $('.main-header .navigation').fadeOut();
+	  enableScroll();
+	});
+});
