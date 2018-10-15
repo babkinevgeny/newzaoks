ab5cb007f358ebd1295d90b08647198e603713be
Evgeny Babkin <babkinevgeny@gmail.com>
2018-10-15 17:21:22 +0300

    Built project

--- dist/js/main.js
+++ dist/js/main.js
@@ -89,4 +89,22 @@ $(document).ready(function(){
 	    $(this).siblings('.container').css('border-bottom', '1px solid #eceded');
 	  }
 	});
+
+ /*Переключение вакансий*/
+  $('.vacancies.team .team__item').click(function(){
+ 		let index = $(this).index();
+ 		if($('.vacancies__info').is(':visible')) {
+ 			$('.vacancies.team .team__item').css('border-bottom','none');
+ 			$('.vacancies.team .team__item').removeClass('team__item--active');
+ 			$(this).addClass('team__item--active');
+ 			$('.vacancies__info_item').fadeOut().delay(500);
+ 			$('.vacancies__info_item').eq(index).fadeIn();
+ 		} else {
+ 			$('.vacancies.team .team__item').css('border-bottom','none');
+ 			$('.vacancies__info').css('display','block');
+ 			$('.vacancies.team .team__item').removeClass('team__item--active');
+ 			$(this).addClass('team__item--active');
+ 			$('.vacancies__info_item').eq(index).fadeIn();
+ 		}
+ 	});
 });
