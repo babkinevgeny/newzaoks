3db070b90087308c6e52f9635812a7df2fdcbebf
Evgeny Babkin <babkinevgeny@gmail.com>
2018-11-30 13:19:30 +0300

    Set switching of texts in logistic page

--- app/js/main.js
+++ app/js/main.js
@@ -218,6 +218,17 @@ $(document).ready(function() {
     let filterValue = $(this).attr('data-filter');
     $grid.isotope({ filter: filterValue });
   });
+
+
+  $('.log-services__item').click(function() {
+    let listBtns = $('.log-services__item');
+    let listTexts = $('.log-descriptions__item');
+    let index = listBtns.index($(this));
+    listBtns.removeClass('active');
+    $(this).addClass('active');
+    listTexts.removeClass('show');
+    listTexts.eq(index).addClass('show');
+  });
 });
 
 function catalogLinks () {
