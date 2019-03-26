9a1c0b8be812a3050d3a15fddac6ef3f10732fb1
Evgeny Babkin <babkinevgeny@gmail.com>
2019-03-26 11:26:44 +0300

    Added test variant of intro section

--- app/js/main.js
+++ app/js/main.js
@@ -4,37 +4,57 @@ $(document).ready(function() {
     auto: true,
     mode: 'fade'
   });
+  counter();
 });
 
-var a = 0;
+let counter = () => {
+  $('.counter__value').each(function() {
+    var $this = $(this),
+      countTo = $this.attr('data-count');
+    $({
+      countNum: $this.text()
+    }).animate({
+        countNum: countTo
+      },
+
+      {
+
+        duration: 2000,
+        easing: 'swing',
+        step: function() {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function() {
+          $this.text(this.countNum);
+          //alert('finished');
+        }
+
+      });
+  });
+}
+
 $(window).scroll(function() {
 
-  var oTop = $('#counter').offset().top - window.innerHeight;
-  if (a == 0 && $(window).scrollTop() > oTop) {
-    $('.counter__value').each(function() {
-      var $this = $(this),
-        countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
-          countNum: countTo
-        },
-
-        {
-
-          duration: 2000,
-          easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-            //alert('finished');
-          }
-
-        });
-    });
-    a = 1;
+  if ($(window).scrollTop() > 300) {
+    $('.main-header--hiding').slideDown();
+  } else{
+    $('.main-header--hiding').slideUp();
   }
 
+});
+
+// var a = 0;
+// $(window).scroll(function() {
+
+//   var oTop = $('#counter').offset().top - window.innerHeight;
+//   if (a == 0 && $(window).scrollTop() > oTop) {
+//     counter();
+//     a = 1;
+//   }
+
+// });
+
+$('.mouse').click(function () {
+  var heightHeader = $('.intro').height();
+   $("body,html").animate({"scrollTop":heightHeader-60},700);
 });
